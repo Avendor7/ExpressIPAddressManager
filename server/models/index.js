dd8c6c8dd68a3a503f9a60ddadb53fe12ed60fdc
step1041 <steve.wiggins7@gmail.com>
2017-02-28 05:01:36 -0500

    fix model index

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 import IpAddress from './IpAddress';
 
 
-//allow all models to be available through the index model
+//allow all models to be available through the index model - so there doesn't need to be a crazy number of imports
 export default {
     IpAddress
 };
